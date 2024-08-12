21c0d2a8656c67e65516f52911dca8eeab7d712a
Prathiksha <145427396+PrathikshaSathish26@users.noreply.github.com>
2024-08-12 08:42:13 +0530

    Delete WEEK 2/PLSQL directory

--- WEEK 2/PLSQL/exercise5scene1.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE TRIGGER UpdateCustomerLastModified
-BEFORE UPDATE ON customers
-FOR EACH ROW
-BEGIN
-    -- Set the LastModified column to the current date and time
-    :NEW.LastModified := SYSDATE;
-END UpdateCustomerLastModified;
-/
